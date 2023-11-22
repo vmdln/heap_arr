11bea38c2053ae57a022fccaf0b7872d3d208306
vmdln <110718729+vmdln@users.noreply.github.com>
2023-11-22 15:46:41 +0100

    Polish docs a bit

--- src/lib.rs
+++ src/lib.rs
@@ -10,7 +10,10 @@ use alloc::{
 
 extern crate alloc;
 
-/// Allocates `[T; N]` on heap and initializes its entries to `default`.
+/// Allocates `[T; N]` on the heap and initializes its entries to `default`.
+///
+/// For now, this function uses the global allocator. This will change once the
+/// [`Allocator`](core::alloc::Allocator) trait becomes stable.
 ///
 /// # Safety
 /// The result of this function is undefined if `mem::size_of::<T>() == 0`, or
@@ -32,15 +35,36 @@ where
     }
 }
 
-/// Allocates `[T; N]` on heap without initializing its entries.
-/// The returned pointer is guaranteed to be non-null
+/// Allocates `[T; N]` on the heap.
+///
+/// The returned pointer is guaranteed to be non-null. The caller is required
+/// to handle deallocation if he wants to avoid memory leaks.
+///
+/// For now, this function uses the global allocator. This will change once the
+/// [`Allocator`](core::alloc::Allocator) trait becomes stable.
 ///
 /// # Safety
-/// The result of this function is undefined if `mem::size_of::<T>() == 0`, or
+/// - The elements are uninitialized
+/// - The result of this function is undefined if `mem::size_of::<T>() == 0`, or
 /// if `N == 0`
 ///
 /// # Errors
 /// See [`Layout::array`]
+///
+/// # Examples
+/// ```
+/// use std::{alloc::{self, Layout}, ptr};
+/// use heap_arr::new_uninit;
+///
+/// unsafe {
+///     let ptr = new_uninit::<usize, 16>().unwrap();
+///     for (i, v) in (*ptr).iter_mut().enumerate() {
+///         ptr::write(v, i);
+///     }
+///
+///     let _ = Box::from_raw(ptr);
+/// }
+/// ```
 pub unsafe fn new_uninit<T, const N: usize>() -> Result<*mut [T; N], LayoutError> {
     let layout = Layout::array::<T>(N)?;
     let ptr = alloc::alloc::alloc(layout);
